48cfe3a4b135bd0c75bb03b2ad9f645db7035b54
Theodoros Papadopoulos <theodoros@earthandfreedom.gr>
2022-05-27 15:50:25 +0300

    Fixed bug in autoloader directories

--- src/autoload.php
+++ src/autoload.php
@@ -6,5 +6,7 @@ $EFL_ROOT = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 require($EFL_ROOT . 'Core/Autoloader.php');
 
 (new \Efl\Core\Autoloader())
-    ->mapNamespace('Efl', $EFL_ROOT . 'src')
+    ->mapNamespace('Efl', $EFL_ROOT)
+    ->mapNamespace('Psr', $EFL_ROOT . 'Psr')
+    ->mapNamespace('Tests', dirname($EFL_ROOT) . '/tests')
     ->register();

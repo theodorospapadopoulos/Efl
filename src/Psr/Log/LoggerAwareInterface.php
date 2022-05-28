cb4e125db2f86e2c49051099063b3dd69be7a563
Theodoros Papadopoulos <theodoros@earthandfreedom.gr>
2022-05-28 17:34:18 +0300

    feat(core): add LoggerAwareInterface for attaching a logger to an object

---
+++ src/Psr/Log/LoggerAwareInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psr\Log;
+
+/**
+ * Represents an entity that can attach and use a logger
+ * as defined in PSR-3
+ *
+ * @author Theodoros Papadopoulos
+ */
+interface LoggerAwareInterface
+{
+    /**
+     * Attach a logger
+     *
+     * @param LoggerInterface $logger
+     * @return void
+     */
+    public function setLogger(LoggerInterface $logger): void;
+}

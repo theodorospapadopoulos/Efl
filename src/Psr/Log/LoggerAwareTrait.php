97a580f63ae81c9fb59b15dac7f7bb992ef50523
Theodoros Papadopoulos <theodoros@earthandfreedom.gr>
2022-05-28 17:48:39 +0300

    feat(core): add LoggerAwareTrait for easy implementation of LoggerAwareInterface

---
+++ src/Psr/Log/LoggerAwareTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psr\Log;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Use to quickly implement the LoggerAwareInterface
+ *
+ * @author Theodoros Papadopoulos
+ */
+class LoggerAwareTrait
+{
+    /**
+     * A PSR-3 logger. Nulls are allowed, as this effectively disables logging
+     *
+     * @var LoggerInterface|null
+     */
+    private ?LoggerInterface $logger = null;
+
+    /**
+     * Attach a logger
+     *
+     * @param LoggerInterface $logger
+     * @return void
+     */
+    public function setLogger(?LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+    }
+}
